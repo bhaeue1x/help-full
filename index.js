641b597fdee6e84194ca66ad3f0c1502df35cf26
bhaeue1x <162807472+bhaeue1x@users.noreply.github.com>
2024-04-08 00:13:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -30,10 +30,30 @@ async function runImage(prompt, urlImage) {
 
 app.get('/', (req, res) => { res.sendFile(__dirname + './views/index.html') })
 
+const arr_what = ['من مطورك', 'من طورك', 'من صنعك', 'من برمجك', 'من اخترعك', 'منو سواك', 'منو اخترعك', 'منو صنعك', 'منو برمجك', 'منو طورك', 'من بشار']
+const arr_dev = ['بشار مرشد الحيوي', 'قام بتطويري بشار حيوي', 'بشار مرشد الحيوي القاطن في الرقة مزرعة ربيعة', 'مطوري بشار مرشد الحيوي', 'قام بإنشائي بشار وبمساعدة من  امجد الخلف']
+const arr_bad = ['مرحبا بك كيف حالك اليوم هل انت بحاجة الى مساعدة انا ذكاء اصطناعي قادر على مساعدتك', 'مرحبا انا نموذج ذكاء اصطناعي تم تطويري بواسطة بشار الحيوي', 'انا ذكاء اصطناعي تم تدريبي بواسطة بشار مرشد الحيوي']
 const err_msg = ['من فضلك حاول صياغة سؤالك بطريقة اخرى', 'اعتذر اني لم افهم سؤالك جيدا', 'يبدو الأتصال ضعيف حاول مجددا']
 app.post('/gemini-text', async (req, res) => {
     try {
         const result = await runText(req.body.message_cont)
+        if (!arr_what.includes(req.body.message_cont)) {
+            if (result == 'err') {
+                res.json({ message_cont: err_msg[Math.floor(Math.random() * err_msg.length - 1) + 1] })
+            } else {
+                if (result.includes("نموذج") && result.includes("لغوي") && result.includes("جوجل")) {
+                    res.json({message_cont: arr_bad[Math.floor(Math.random() * arr_bad.length - 1) + 1]})
+                } else {res.json({ message_cont: result })}
+            }
+        } else { res.json({ message_cont: arr_dev[Math.floor(Math.random() * arr_dev.length - 1) + 1] }) }
+
+    } catch (err) { console.log('err') }
+})
+
+app.post('/gemini-image', upload.single('image'), async (req, res) => {
+    const buffer = await req.file.buffer.toString('base64')
+    try {
+        const result = await runImage(req.body.prompt, buffer)
         if (result == 'err') {
             res.json({ message_cont: err_msg[Math.floor(Math.random() * err_msg.length - 1) + 1] })
         } else {
@@ -42,17 +62,5 @@ app.post('/gemini-text', async (req, res) => {
     } catch (err) { console.log('err') }
 })
 
-app.post('/gemini-image', upload.single('image'),  async (req, res) => {
-   const buffer = await req.file.buffer.toString('base64')
-    try {
-        const result = await runImage(req.body.prompt, buffer)
-        if(result == 'err') {
-            res.json({ message_cont: err_msg[Math.floor(Math.random() * err_msg.length - 1) + 1] })
-        } else {
-            res.json({ message_cont: result })
-        }
-    } catch (err) {console.log('err')}
-})
-
-app.get('/*', (req, res) => { res.send('thes bage none3 ...') })
+app.get('/*', (req, res) => { res.send('thes bage none2 ...') })
 app.listen(process.env.PORT || 3000, () => { console.log(`app listen now ...`) })
